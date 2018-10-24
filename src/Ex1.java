d120046346c99681240ce8531bb1242f108076b0
mlukasik93 <mlukasik93@gmail.com>
2018-10-24 23:41:21 +0200

    Final version

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,8 +1,9 @@
 import static java.lang.Math.abs;
+import static java.lang.Math.cbrt;
 
 public class Ex1 {
 
-    static int[] arr = {1, 2, 3, 4, 5, 6, 7,8,10};
+    static int[] arr = {6,7,8,9,10,1,2,3,5};
 
     public static void main(String[] args) {
         System.out.println("The missing number is: " + findMissingNumber(arr));
@@ -11,11 +12,17 @@ public class Ex1 {
     public static int findMissingNumber(int[] arr) {
         int firstNumber = 0;
         int secondNumber = 1;
-        while (abs(arr[firstNumber] - arr[secondNumber]) == 1) {
-            firstNumber++;
-            secondNumber++;
+        for (int i = 0; i+1 < arr.length; i++) {
+            switch (arr[firstNumber] - arr[secondNumber]) {
+                case 8:
+                    return 1;
+                case -2:
+                    return arr[firstNumber]+1;
+                default:
+                    firstNumber++;
+                    secondNumber++;
+            }
         }
-        return secondNumber;
-
+        return 0;
     }
 }
