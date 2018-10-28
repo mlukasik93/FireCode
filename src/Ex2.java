6b72d9ad24e7c2e7ab513092800dfe793c6c8fec
mlukasik93 <mlukasik93@gmail.com>
2018-10-28 23:36:41 +0100

    It works

--- src/Ex2.java
+++ src/Ex2.java
@@ -1,14 +1,34 @@
 public class Ex2 {
     public static void main(String[] args) {
-        int [] matrix = {1,5,9,7};
+        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}};
         flipItVerticalAxis(matrix);
     }
 
-
-    public static void flipItVerticalAxis(int[] matrix) {
-           for (int i=0; i<matrix.length;i++){
-               matrix[i]=matrix[matrix.length-1-i];
-               System.out.print(matrix[i]+", ");
-           }
+    private static void flipItVerticalAxis(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            int[][] tempMatrix = new int[matrix.length][matrix[0].length];
+            for (int j = 0; j < matrix[0].length; j++) {
+                tempMatrix[i][j] = matrix[i][j];
+            }
+            if (matrix[0].length % 2 == 0) {
+                for (int j = 0; j < matrix[0].length / 2; j++) {
+                    matrix[i][j] = matrix[i][matrix[0].length - 1 - j];
+                    System.out.print(matrix[i][j] + " ");
+                }
+                for (int j = matrix[0].length / 2; j < matrix[0].length; j++) {
+                    matrix[i][j] = tempMatrix[i][matrix[0].length - 1 - j];
+                    System.out.print(matrix[i][j] + " ");
+                }
+            } else {
+                for (int j = 0; j < matrix[0].length / 2; j++) {
+                    matrix[i][j] = matrix[i][matrix[0].length - 1 - j];
+                    System.out.print(matrix[i][j] + " ");
+                }
+                for (int j = matrix[0].length / 2; j < matrix[0].length; j++) {
+                    matrix[i][j] = tempMatrix[i][matrix[0].length - 1 - j];
+                    System.out.print(matrix[i][j] + " ");
+                }
+            }
+        }
     }
 }
