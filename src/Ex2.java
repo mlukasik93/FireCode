cdd2648b083aedc9ec70aecb2d6422af86c85524
mlukasik93 <mlukasik93@gmail.com>
2018-10-28 10:41:46 +0100

    why {7,9,9,7} ?

--- src/Ex2.java
+++ src/Ex2.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Ex2 {
+    public static void main(String[] args) {
+        int [] matrix = {1,5,9,7};
+        flipItVerticalAxis(matrix);
+    }
+
+
+    public static void flipItVerticalAxis(int[] matrix) {
+           for (int i=0; i<matrix.length;i++){
+               matrix[i]=matrix[matrix.length-1-i];
+               System.out.print(matrix[i]+", ");
+           }
+    }
 }
