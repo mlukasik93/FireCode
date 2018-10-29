80ff6606583aeb4fa2e85a84bf1c0ad1b331445c
mlukasik93 <mlukasik93@gmail.com>
2018-10-30 00:49:58 +0100

    On Intellij works. On FireCode doesn't

--- src/Ex4.java
+++ src/Ex4.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Ex4 {
+
+    public static void main(String[] args) {
+        String a = "Replace all spaces";
+        String b = "ABCD";
+        System.out.println(replace(a, b));
+    }
+
+    public static String replace(String a, String b) {
+        List<Character> list = new ArrayList<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        String result=null;
+        int i = 0;
+        while (i < a.length()) {
+            if (a.charAt(i) != ' ') {
+                list.add(a.charAt(i));
+            } else {
+                for (int j = 0; j < b.length(); j++) {
+                    list.add(b.charAt(j));
+                }
+            }
+            i++;
+        }
+        for (Character n : list) {
+            result=stringBuilder.append(n).toString();
+        }
+
+        return result;
+    }
+
 }
